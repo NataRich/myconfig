c3b1833af7a4c714a39fb41ce32eebc8626c7f49
Zihan Zhao <zzhao383@wisc.edu>
2023-06-10 15:21:42 +0800

    Add more language servers

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,11 +9,41 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls" }
+  ensure_installed = { "lua_ls", "gopls", "clangd", "tsserver",
+  "cmake", "jedi_language_server", "dockerls",
+  "docker_compose_language_service"}
 })
 
 local capabilities =  require('cmp_nvim_lsp').default_capabilities()
 
 require('lspconfig').lua_ls.setup {
   capabilities = capabilities,
+}
+
+require('lspconfig').gopls.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').clangd.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').tsserver.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').cmake.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').jedi_language_server.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').dockerls.setup {
+  capabilities = capabilities
+}
+
+require('lspconfig').docker_compose_language_service.setup {
+  capabilities = capabilities
 }
